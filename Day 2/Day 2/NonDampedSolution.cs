17494a0d6dbe4df8111596cc08dbbfb8323e511e
Elliot <elliots1m@hotmail.co.uk>
2024-12-02 07:12:00 +0000

    Day 2

---
+++ Day 2/Day 2/NonDampedSolution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Day_2
+{
+    public class NonDampedSolution
+    {
+        public static bool Solve(int[] numArray, out int failedIndex)
+        {
+            failedIndex = -1;
+            int[] pSumList = new int[numArray.Length];
+            bool first = true;
+            bool valid = true;
+            bool ascending;
+            for (int i = 1; i < numArray.Length; i++)
+            {
+                pSumList[i - 1] = (numArray[i] - numArray[i - 1]);
+                if (Math.Abs(numArray[i] - numArray[i - 1]) > 3 || numArray[i] == numArray[i - 1])
+                {
+                    valid = false;
+                    failedIndex = i;
+                    break;
+                }
+            }
+
+            if (!valid)
+            {
+                return false;
+            }
+
+            if (pSumList.First() > 0)
+            {
+                ascending = true;
+            }
+            else
+            {
+                ascending = false;
+            }
+
+            for (int i = 0; i < pSumList.Length; i++)
+            {
+                if ((ascending && pSumList[i] < 0) || (!ascending && pSumList[i] > 0))
+                {
+                    valid = false;
+                    failedIndex = i + 1;
+                    break;
+                }
+            }
+
+            if (valid)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Day 2/Day 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using Day_2;
+
+string[] lines = Input.INPUT.Split('\n');
+int correctReports = 0;
+foreach (string line in lines)
+{
+    int[] numArray = line.Split(" ").Select(x => int.Parse(x)).ToArray();
+    int failedIndex = -1;
+    if (NonDampedSolution.Solve(numArray, out failedIndex))
+    {
+        correctReports++;
+    }
+    else
+    {
+        int[] removeAtIndex = new int[numArray.Length - 1];
+        int[] removeBeforeIndex = new int[numArray.Length - 1];
+        int[] removeFirst = numArray.Reverse().Take(numArray.Length - 1).Reverse().ToArray(); //Edge case where this reverses whether we are increasing or decreasing
+        for (int i = 0; i < numArray.Length; i++)
+        {
+            if (i < failedIndex - 1)
+            {
+                removeBeforeIndex[i] = numArray[i];
+                removeAtIndex[i] = numArray[i];
+            }
+            else if (i == failedIndex - 1)
+            {
+                removeAtIndex[i] = numArray[i];
+            }
+            else if (i == failedIndex)
+            {
+                removeBeforeIndex[i - 1] = numArray[i];
+            }
+            else if (i > failedIndex)
+            {
+                removeBeforeIndex[i - 1] = numArray[i];
+                removeAtIndex[i - 1] = numArray[i];
+            }
+        }
+        if (
+            NonDampedSolution.Solve(removeAtIndex, out _)
+            || NonDampedSolution.Solve(removeBeforeIndex, out _)
+            || NonDampedSolution.Solve(removeFirst, out _)
+        )
+        {
+            correctReports++;
+        }
+    }
+}
+
+Console.WriteLine(correctReports);
